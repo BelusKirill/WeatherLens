8a076d4f063ef12c57c42a593a32110196aa5763
BelusKirill <beluskirill97@gmail.com>
2026-08-29 19:40:33 +0300

    #4 expose shared weather search and forecast helpers

--- src/features/favorites/index.ts
+++ src/features/favorites/index.ts
@@ -1,3 +1,4 @@
 export { FavoritesScreen } from './ui/FavoritesScreen';
 export { FavoriteToggle } from './ui/FavoriteToggle';
 export { useFavoritesStore } from './model/favoritesStore';
+export { isSameLocation } from './model/favorites';
